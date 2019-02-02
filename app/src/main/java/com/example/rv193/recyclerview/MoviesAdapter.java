fb232d938412ba9c50b23766136cc7c2d545deb0
Ricardo Viera <rvscript@gmail.com>
2019-02-02 15:19:49 -0500

    Recyclerview RV- basic recyclerview first commit

---
+++ app/src/main/java/com/example/rv193/recyclerview/MoviesAdapter.java
@@ -0,0 +1,50 @@
+package com.example.rv193.recyclerview;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.List;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHolder> {
+
+    private List<Movie> movieList;
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_list_row,
+                parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Movie movie = movieList.get(position);
+        holder.title.setText(movie.getTitle());
+        holder.genre.setText(movie.getGenre());
+        holder.year.setText(movie.getYear());
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, year, genre;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.tv_title);
+            genre = itemView.findViewById(R.id.genre);
+            year = itemView.findViewById(R.id.year);
+        }
+    }
+
+    public MoviesAdapter(List<Movie> moviesList) {
+        this.movieList = moviesList;
+    }
+}
